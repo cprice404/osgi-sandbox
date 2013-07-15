67e633ade75deba3d9bb44cf1a09f76dbbf70392
Chris Price <chris@puppetlabs.com>
2013-07-14 20:31:48 -0700

    Simplified webservice to leverage http-whiteboard for registration

--- osgi-bundle-sandbox/src/main/java/com/puppetlabs/sandbox/osgi/http/HelloWorldWebService.java
+++ osgi-bundle-sandbox/src/main/java/com/puppetlabs/sandbox/osgi/http/HelloWorldWebService.java
@@ -1,12 +1,10 @@
 package com.puppetlabs.sandbox.osgi.http;
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.*;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component(immediate = true)
-@Service
+@Service(value = Servlet.class)
+@Properties({
+        @Property(name = "alias", value = "/hello2")
+})
 public class HelloWorldWebService extends HttpServlet {
 
-    @Reference
-    private HttpService httpService;
+//    @Reference
+//    private HttpService httpService;
 
     public void HelloWorldWebService() {
         System.out.println("HELLO WORLD WEB SERVICE: INITIALIZING");
@@ -35,19 +36,19 @@ public class HelloWorldWebService extends HttpServlet {
             throws ServletException, IOException {
         resp.getWriter().write("Hello World");
     }
-
-    protected void bindHttpService(HttpService service) throws ServletException, NamespaceException {
-        System.out.println("BINDING HTTP SERVICE");
-        this.httpService = service;
-
-
-        this.httpService.registerServlet("/hello",
-                this, null, null);
-    }
-
-    protected void unbindHttpService(HttpService service) {
-        System.out.println("UNBINDING HTTP SERVICE");
-        service.unregister("/hello");
-        this.httpService = null;
-    }
+//
+//    protected void bindHttpService(HttpService service) throws ServletException, NamespaceException {
+//        System.out.println("BINDING HTTP SERVICE");
+//        this.httpService = service;
+//
+//
+//        this.httpService.registerServlet("/hello",
+//                this, null, null);
+//    }
+//
+//    protected void unbindHttpService(HttpService service) {
+//        System.out.println("UNBINDING HTTP SERVICE");
+//        service.unregister("/hello");
+//        this.httpService = null;
+//    }
 }
