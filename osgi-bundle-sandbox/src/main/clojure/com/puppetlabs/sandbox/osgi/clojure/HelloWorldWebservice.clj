27baf2a446fbb8e53872c11c0a32149289b13766
Chris Price <chris@puppetlabs.com>
2013-07-21 12:57:13 -0700

    Realized that clojure bundle will require more manifest headers

--- osgi-bundle-sandbox/src/main/clojure/com/puppetlabs/sandbox/osgi/clojure/HelloWorldWebservice.clj
+++ osgi-bundle-sandbox/src/main/clojure/com/puppetlabs/sandbox/osgi/clojure/HelloWorldWebservice.clj
@@ -1,26 +1,28 @@
-(ns com.puppetlabs.sandbox.osgi.clojure.HelloWorldWebservice
-;  (:import [org.apache.felix.scr.annotations Component Service])
-  (:import [javax.servlet Servlet]
-           [com.puppetlabs.sandbox.osgi.annotation PropertyLiteral])
-;  (:gen-class
-;    :name ^{org.apache.felix.scr.annotations.Component []
-;            org.apache.felix.scr.annotations.Service []}
-;      com.puppetlabs.sandbox.osgi.clojure.HelloWorldWebservice
-;    :extends javax.servlet.http.HttpServlet)
-  (:use [ring.util.servlet :only [defservice]]))
-
-(gen-class
-  :name ^{org.apache.felix.scr.annotations.Component {:immediate true}
-          org.apache.felix.scr.annotations.Service   {:value [Servlet]}
-          org.apache.felix.scr.annotations.Properties []}
-          ;(PropertyLiteral. "alias" ["/helloclojure"])]}
-;            [org.apache.felix.scr.annotations.Property {:name "alias"
-;                                                        :value "/helloclojure"}]}
-
-    com.puppetlabs.sandbox.osgi.clojure.HelloWorldWebservice.Foo
-  :extends javax.servlet.http.HttpServlet
-  :prefix "foo-")
-
-(defservice "foo-"
-  (fn [req]
-    {:body "hello clojure world!"}))
+;(ns com.puppetlabs.sandbox.osgi.clojure.HelloWorldWebservice
+;;  (:import [org.apache.felix.scr.annotations Component Service])
+;  (:import [javax.servlet Servlet])
+;;  (:gen-class
+;;    :name ^{org.apache.felix.scr.annotations.Component []
+;;            org.apache.felix.scr.annotations.Service []}
+;;      com.puppetlabs.sandbox.osgi.clojure.HelloWorldWebservice
+;;    :extends javax.servlet.http.HttpServlet)
+;  (:use [ring.util.servlet :only [defservice]]))
+;
+;(gen-class
+;  :name ^{org.osgi.service.component.annotations.Component
+;            {:service     [Servlet]
+;             :immediate   true
+;             :property    ["alias=/hello-clojure"]}}
+;;          org.apache.felix.scr.annotations.Service   {:value [Servlet]}
+;;          org.apache.felix.scr.annotations.Properties []}
+;          ;(PropertyLiteral. "alias" ["/helloclojure"])]}
+;;            [org.apache.felix.scr.annotations.Property {:name "alias"
+;;                                                        :value "/helloclojure"}]}
+;
+;    com.puppetlabs.sandbox.osgi.clojure.HelloWorldWebservice.Foo
+;  :extends javax.servlet.http.HttpServlet
+;  :prefix "foo-")
+;
+;(defservice "foo-"
+;  (fn [req]
+;    {:body "hello clojure world!"}))
